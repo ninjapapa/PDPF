bc383b945c77aa369532c61d6d74125bbdb48501
Bo Zhang <bzhangusc@live.com>
2020-09-28 09:57:04 -0700

    #7 added logger

---
+++ src/main/python/pdpf/__init__.py
@@ -0,0 +1,11 @@
+import logging
+logger = logging.getLogger(__name__)
+# Default logging settings
+# These may be overridden when SmvApp initializes
+log_formatter = logging.Formatter(
+    fmt='%(asctime)s %(levelname)s %(name)s: %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S')
+stderr_log_appender = logging.StreamHandler()
+stderr_log_appender.setFormatter(log_formatter)
+logger.addHandler(stderr_log_appender)
+logger.setLevel("INFO")
